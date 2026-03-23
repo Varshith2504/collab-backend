37c77c3469a6b367533cabc95aff23fd964bfee5
Varshith <ellendulavarshith1@gmail.com>
2026-03-23 20:31:29 +0530

    update variables

--- src/main/java/com/collab/config/CorsConfig.java
+++ src/main/java/com/collab/config/CorsConfig.java
@@ -1,14 +1,11 @@
 package com.collab.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
